f8bd77aa277d92eff797f197f608c72371af5ce3
Srivarun05 <varunsiva47@gmail.com>
2026-03-16 13:00:48 +0530

    added context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 
 function App() {
   return (
-    <BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
       <Routes>
-        {/* Automatically redirect the root URL to the login page */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        
-        {/* Auth Routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        
-        {/* You will add your Dashboard/Home route here later! */}
-        {/* <Route path="/home" element={<Home />} /> */}
-      </Routes>
-    </BrowserRouter>
+          {/* Automatically redirect the root URL to the login page */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          
+          {/* Auth Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          
+          {/* You will add your Dashboard/Home route here later! */}
+          {/* <Route path="/home" element={<Home />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>  
+
   );
 }
 
